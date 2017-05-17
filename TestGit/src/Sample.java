74df5e2bdeea7755da862c1d4ac57eb12d20232b
up4574 <up4574@DY0HG32.DDNET1.net>
2017-05-17 10:08:21 +0530

    removed test checkin output statement

--- TestGit/src/Sample.java
+++ TestGit/src/Sample.java
@@ -4,6 +4,6 @@ public class Sample {
 	public static void main(String[] args){
 	System.out.println("Welcome");
 	System.out.println("Again Welcome");
-	System.out.println("test check in again");
+	
 }
 }
